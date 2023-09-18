75532ac27765f57acc42475fc39a3cc9a67a71d3
Ozgun Aksoy <zgnaksoy@gmail.com>
2023-09-18 14:07:40 -0700

    Initial Commit

---
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUIApp.swift
+//  InstagramSwiftUI
+//
+//  Created by özgün aksoy on 2023-09-18.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
