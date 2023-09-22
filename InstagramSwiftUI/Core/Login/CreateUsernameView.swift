3ba79851cce4f80af6e76fa2c2512432dc46e478
Ozgun Aksoy <zgnaksoy@gmail.com>
2023-09-21 23:36:26 -0700

    add create username view

---
+++ InstagramSwiftUI/Core/Login/CreateUsernameView.swift
@@ -0,0 +1,65 @@
+//
+//  CreateUsernameView.swift
+//  InstagramSwiftUI
+//
+//  Created by özgün aksoy on 2023-09-21.
+//
+
+import SwiftUI
+
+struct CreateUsernameView: View {
+    @State private var username: String = ""
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            Text("Create a Username")
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.top)
+            
+            
+            Text("Pick a username for your new account. You can always change it later.")
+                .font(.footnote)
+                .foregroundStyle(.gray)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 24)
+            
+            TextField("Username", text: $username)
+                .textInputAutocapitalization(.never)
+                .modifier(IGTextFieldModifier())
+            
+            
+            NavigationLink {
+                PasswordView()
+                    .navigationBarBackButtonHidden()
+            } label: {
+                Text("Next")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(width: 360, height: 44)
+                    .background(.blue.gradient)
+                    .clipShape(.rect(cornerRadius: 8))
+            }
+            .padding(.vertical)
+            
+            Spacer()
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Image(systemName: "chevron.left")
+                    .imageScale(.large)
+                    .onTapGesture {
+                        dismiss()
+                    }
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        CreateUsernameView()
+    }
+}
