d69624f9ce78c0bd982cafad03d13d4205e92ac6
Ozgun Aksoy <zgnaksoy@gmail.com>
2023-09-21 23:36:16 -0700

    add add email view

---
+++ InstagramSwiftUI/Core/Login/AddEmailView.swift
@@ -0,0 +1,65 @@
+//
+//  AddEmailView.swift
+//  InstagramSwiftUI
+//
+//  Created by özgün aksoy on 2023-09-21.
+//
+
+import SwiftUI
+
+struct AddEmailView: View {
+    @State private var email: String = ""
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            Text("Add Your Email")
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.top)
+            
+            
+            Text("You'll use this email to sign in to your account")
+                .font(.footnote)
+                .foregroundStyle(.gray)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 24)
+            
+            TextField("Email", text: $email)
+                .textInputAutocapitalization(.never)
+                .modifier(IGTextFieldModifier())
+            
+            
+            NavigationLink {
+                CreateUsernameView()
+                    .navigationBarBackButtonHidden()
+            } label: {
+                Text("Next")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(width: 360, height: 44)
+                    .background(.blue.gradient)
+                    .clipShape(.rect(cornerRadius: 8))
+            }
+            .padding(.vertical)
+            
+            Spacer()
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Image(systemName: "chevron.left")
+                    .imageScale(.large)
+                    .onTapGesture {
+                        dismiss()
+                    }
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        AddEmailView()
+    }
+}
