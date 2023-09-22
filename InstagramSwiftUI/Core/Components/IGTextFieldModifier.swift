397dc9fb28591d124aff0ec5962a9d571451ad6d
Ozgun Aksoy <zgnaksoy@gmail.com>
2023-09-21 23:37:07 -0700

    add custom text field modifier

---
+++ InstagramSwiftUI/Core/Components/IGTextFieldModifier.swift
@@ -0,0 +1,19 @@
+//
+//  IGTextFieldModifier.swift
+//  InstagramSwiftUI
+//
+//  Created by özgün aksoy on 2023-09-21.
+//
+
+import SwiftUI
+
+struct IGTextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .padding(12)
+            .background(Color(.systemGray6))
+            .clipShape(.rect(cornerRadius: 10))
+            .padding(.horizontal, 24)
+    }
+}
